9f7d0abd4f05e95d26c3e1b9a3831d5d58de5a88
NikkLZ <nicole.zelingher@gmail.com>
2018-02-06 10:19:22 -0500

    edits

---
+++ connect4.js
@@ -0,0 +1,42 @@
+let A = document.getElementById("A");
+function printA(){
+  console.log("Place piece in colomn A?");
+  }
+A.addEventListener("click",printA);
+
+let B = document.getElementById("B");
+function printB(){
+  console.log("Place piece in colomn B?");
+  }
+B.addEventListener("click",printB);
+
+let C = document.getElementById("C");
+function printC(){
+  console.log("Place piece in colomn C?");
+  }
+C.addEventListener("click",printC);
+
+let D = document.getElementById("D");
+function printD(){
+  console.log("Place piece in colomn D?");
+  }
+D.addEventListener("click",printD);
+
+let E = document.getElementById("E");
+function printE(){
+  console.log("Place piece in colomn E?");
+  }
+E.addEventListener("click",printE);
+
+let F = document.getElementById("F");
+function printF(){
+  console.log("Place piece in colomn F?");
+  }
+F.addEventListener("click",printF);
+
+let G = document.getElementById("G");
+function printG(){
+  console.log("Place piece in colomn G?");
+  }
+G.addEventListener("click",printG);
+
